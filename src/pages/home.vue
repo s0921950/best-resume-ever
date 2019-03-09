eaa205bfb7df6832dc694a3061ce212091631e74
Adam <adam.yh.tung@mail.foxconn.com>
2019-03-09 15:00:06 +0800

    Modified home page.

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="home">
-  <a href="https://github.com/salomonelli/best-resume-ever" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>
+  <!--<a href="https://github.com/salomonelli/best-resume-ever" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>-->
   <div class="logo">
     <img src="../assets/logo.png" />
   </div>
-  <h3 class="title">best-resume-ever</h3>
+  <h3 class="title">My resume</h3>
   <div class="previews">
     <div class="preview">
       <router-link v-bind:to="'/resume/material-dark'">
@@ -22,14 +22,14 @@
         </div>
       </router-link>
     </div>
-    <div class="preview">
+    <!-- <div class="preview">
       <router-link v-bind:to="'/resume/oblique'">
         <div class="preview-wrapper">
           <img src="../assets/preview/resume-oblique.png" />
           <span>oblique</span>
         </div>
       </router-link>
-    </div>
+    </div> -->
     <div class="preview">
       <router-link v-bind:to="'/resume/side-bar'">
         <div class="preview-wrapper">
@@ -46,7 +46,7 @@
         </div>
       </router-link>
     </div>
-    <div class="preview">
+    <!-- <div class="preview">
       <router-link v-bind:to="'/resume/side-bar-rtl'">
         <div class="preview-wrapper">
           <img src="../assets/preview/resume-side-bar-rtl.png" />
@@ -69,12 +69,23 @@
           <span>creative</span>
         </div>
       </router-link>
-    </div>
+    </div> -->
     <div class="preview">
       <router-link v-bind:to="'/resume/cool'">
         <div class="preview-wrapper">
           <img src="../assets/preview/resume-cool.png" />
           <span>cool</span>
+        </div>
+      </router-link>
+    </div>
+  </div>
+  <h3 class="title">books</h3>
+  <div class="previews">
+    <div class="preview">
+      <router-link v-bind:to="'/resume/material-dark'">
+        <div class="preview-wrapper">
+          <img src="../assets/preview/resume-material-dark.png" />
+          <span>material-dark</span>
         </div>
       </router-link>
     </div>
@@ -91,7 +102,7 @@ export default Vue.component('resume', {
 
 <style scoped>
 .home {
-  font-family: 'Roboto' !important;
+  font-family: 'Roboto';
 }
 
 .logo {
@@ -116,10 +127,19 @@ export default Vue.component('resume', {
   margin-bottom: 40px;
 }
 
-.previews {
+.books {
+  text-align: center;
   width: 90%;
   margin-right: auto;
   margin-left: auto;
+}
+
+.previews {
+  width: 90%;
+  height: 800px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: 40px;
 }
 
 .preview {
@@ -156,4 +176,46 @@ export default Vue.component('resume', {
   top: 50%;
   transform: translateY(-50%);
 }
+
+@media (min-width:340px){
+  .previews {
+    width: 190px;
+    height: 800px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 40px;
+  }
+}
+
+@media (min-width:580px){
+  .previews {
+    width: 384px;
+    height: 800px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 40px;
+  }
+}
+
+@media (min-width:820px){
+  .previews {
+    width: 820px;
+    height: 800px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 40px;
+  }
+}
+
+@media (min-width:1060px){
+  .previews {
+    width: 1060px;
+    height: 800px;
+    margin-right: auto;
+    margin-left: auto;
+    margin-bottom: 40px;
+  }
+}
+
+
 </style>
